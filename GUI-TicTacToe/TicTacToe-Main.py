404d3cb3c0cd14ff6670331be1f719ebe17c943c
Arun Shrestha <76599239+arun-k-shrestha@users.noreply.github.com>
2021-06-11 14:11:26 -0700

    Update TicTacToe-Main.py

--- GUI-TicTacToe/TicTacToe-Main.py
+++ GUI-TicTacToe/TicTacToe-Main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import numpy
 
 pygame.init()
 
@@ -6,12 +7,18 @@ WIDTH = 660
 HEIGHT = 660
 BG = (30, 160, 150 ) #background color
 
+ROWS = 3
+COLS = 3
+
 LINE_COLOR = (20,140,130)
 LINE_WIDTH = 15
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('TicTacToe Game')
 screen.fill( BG )
+
+board = numpy.zeros((3,3))
+print(board)
 
 def line():
     #horizontal lines
@@ -21,6 +28,16 @@ def line():
     #vertical lines
     pygame.draw.line(screen,LINE_COLOR, (220,0),(220,660), LINE_WIDTH)
     pygame.draw.line(screen,LINE_COLOR, (440,0),(440,660), LINE_WIDTH)
+
+def marking(row, col, mark):
+    board[row][col] = mark
+
+def is_full():
+    for row in range(ROWS):
+        for col in range(COLS):
+            if board[row][col]==0:
+                return False
+    return True
 
 line()
 
