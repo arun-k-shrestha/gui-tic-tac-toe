9669b60a8a96565ffff67c39e2191b10bd0eb50b
Arun Shrestha <76599239+arun-k-shrestha@users.noreply.github.com>
2021-06-29 19:11:53 -0700

    Update TicTacToe-Main.py

--- GUI-TicTacToe/TicTacToe-Main.py
+++ GUI-TicTacToe/TicTacToe-Main.py
@@ -4,13 +4,14 @@ import functions
 
 functions.line()
 player = 1
+game_over = True
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and game_over == True:
 
             mouseX = event.pos[0]
             mouseY = event.pos[1]
@@ -29,6 +30,14 @@ while True:
                 functions.drawing()
                 functions.diagonal_lines()
                 functions.winning_line()
+
+                if functions.winning_line() == True or functions.diagonal_lines() == True:
+                    game_over = False
+
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_r:
+        #         functions.restart()
+        
     
     pygame.display.update()
 
